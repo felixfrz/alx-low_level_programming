536e130f14230755ae8a0f2305b3d78fa1f6edd0
felixfrz <oladoyinayodeji@gmail.com>
2022-11-17 18:05:44 +0000

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,30 +1,57 @@
-#include "variadic_functions.h"
+// #include "variadic_functions.h"
+// #include <stdarg.h>
+
+// /**
+// *sum_them_all - sums all its parameters
+// *@n: first parameter
+// *
+// *Return: 0 if n is null
+// * or sum of parameters in other case
+// */
+
+// int sum_them_all(const unsigned int n, ...)
+// {
+
+// va_list li;
+// int sum = 0;
+// unsigned int i;
+
+// va_start(li, n);
+
+// if (n != 0)
+// {
+// for (i = 0; i < n; i++)
+// sum += va_arg(li, int);
+// }
+
+// va_end(li);
+// return (sum);
+
+// }
+
+
+#include <stdio.h>
 #include <stdarg.h>
 
 /**
-*sum_them_all - sums all its parameters
-*@n: first parameter
-*
-*Return: 0 if n is null
-* or sum of parameters in other case
+* sum_them_all - sum of all its parameters
+* @n: n args
+* Return: sum or if n == 0, return 0
 */
-
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum = 0;
+	unsigned int i;
+	va_list arguments;
 
-va_list li;
-int sum = 0;
-unsigned int i;
-
-va_start(li, n);
-
-if (n != 0)
-{
-for (i = 0; i < n; i++)
-sum += va_arg(li, int);
-}
-
-va_end(li);
-return (sum);
-
+	if (n)
+	{
+		va_start(arguments, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(arguments, int);
+		}
+		va_end(arguments);
+	}
+	return (sum);
 }
